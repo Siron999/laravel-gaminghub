ded7f53c0885bf76b4e2a84203b2549c8e49397b
Siron <sironshakya10@gmail.com>
2021-09-06 19:46:50 +0545

    esewa payment added

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -11,22 +11,22 @@ use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-      
+
     public function index(){
-        
+
 
 
         return view('welcome');
-    
+
             }
 
-    
+
 
 
     public function show($id){
-//checking json data representation 
+//checking json data representation
         $all=Game::whereJsonContains('genre', $id)->get();
-        
+
 
 
         return view('genre',['id'=>$all],['title'=>strtoupper($id)]);
@@ -34,9 +34,9 @@ class GameController extends Controller
 
     }
     public function show2($id){
-//checking json data representation 
+//checking json data representation
         $all=Product::whereJsonContains('type', $id)->get();
-        
+
 
 
         return view('type',['id'=>$all],['title'=>strtoupper($id)]);
@@ -46,18 +46,18 @@ class GameController extends Controller
 
     public function details($id){
                 $all=Game::where('id', $id)->get();
-                
-        
-        
+
+
+
                 return view('gamedetails',['id'=>$all]);
-        
-        
+
+
             }
 
-            
+
     public function details2($id){
         $all=Product::where('id', $id)->get();
-        
+
 
 
         return view('productdetails',['id'=>$all]);
@@ -89,7 +89,7 @@ class GameController extends Controller
         return back();
 
     }
-   
+
 
 //adding item to the cart
 
@@ -97,29 +97,29 @@ class GameController extends Controller
         $game= Game::find($id);
         if($game==null){
         $game= Product::find($id);
-        }   
+        }
         $oldCart=Session::has('cart') ? Session::get('cart') :null;
         $cart= new Cart($oldCart);
         $cart->add($game,$game->id);
-        
-        
+
+
         $request->session()->put('cart',$cart);
         //dd($request->session()->get('cart'));
         return back();
 
 
-    }   
+    }
     // for the ceckout view
     public function cart(){
         if(!Session::has('cart')){
             return view('checkout');
 
         }
-       
+
         $oldCart=Session::has('cart') ? Session::get('cart') :null;
         $cart= new Cart($oldCart);
         return view('checkout',['games'=>$cart->items,'totalPrice'=>$cart->totalPrice,'totalQty'=>$cart->totalQty]);
-     
+
     }
 
     //deleting single quanity  from item in cart
@@ -127,8 +127,8 @@ class GameController extends Controller
         $oldCart=Session::get('cart');
         $cart= new Cart($oldCart);
         $cart->delete($id);
-        
-        
+
+
         Session()->put('cart',$cart);
         //dd($request->session()->get('cart'));
         return back();
@@ -140,8 +140,8 @@ class GameController extends Controller
         $oldCart=Session::get('cart');
         $cart= new Cart($oldCart);
         $cart->deleteall($id);
-        
-        
+
+
         Session()->put('cart',$cart);
         //dd($request->session()->get('cart'));
         return back();
--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -6,16 +6,16 @@
 <div class="container" style="color:#353535;margin-top:80px;">
 <div class="container container-custom mb-2"> <h2><b>Checkout</b></h2><hr> </div>
         @if(Session::has('cart'))
-      
+
         <div class="row" >
         <div class="col" >
-       
+
               <ul class="list-group">
               @foreach($games as $game)
                 <li class="list-group-item">
                    <strong>{{$game['item']['name']}}</strong> <br>
-                   <strong>Price: {{$game['price']}}</strong> 
-                   
+                   <strong>Price: {{$game['price']}}</strong>
+
                    <span class="badge badge-secondary" style="float:right;">{{$game['qty']}}</span>
                    <form action="/removefromcart/{{$game['item']['id']}}" method="POST">
                    @csrf
@@ -27,17 +27,17 @@
                     @method('DELETE')
                    <button class="btn btn-custom3 btn-danger " >Remove All</button>
                   </form>
-                  </li> 
-             
+                  </li>
+
                   @endforeach
-               </ul>   
+               </ul>
     <div class="mt-5">
     <div class="row p-0 "  >
         <div class="col-9" >
        <strong> <label class="col-2 p-0" >Total Quantity:</label> {{$totalQty}}<strong>
         </div>
     </div>
-  
+
     <div class="row p-0">
         <div class="col-9" >
         <strong><label class="col-2 p-0">Total Price:</label>{{$totalPrice}}</strong>
@@ -45,27 +45,23 @@
     </div>
     <div class="row p-0 "  >
         <div class="col-9" >
-        <a href="#" class="btn btn-custom btn-danger " >Checkout</a>
+         <form action="https://uat.esewa.com.np/epay/main" method="POST">
+                <input value="{{$totalPrice + 10}}" name="tAmt" type="hidden">
+                    <input value="{{$totalPrice}}" name="amt" type="hidden">
+                    <input value="10" name="txAmt" type="hidden">
+                    <input value="0" name="psc" type="hidden">
+                    <input value="0" name="pdc" type="hidden">
+                    <input value="testmerchant" name="scd" type="hidden">
+                    <input value="121234" name="pid" type="hidden">
+                    <input value="http://merchantsite.com/success,html?q=su" type="hidden" name="su">
+                    <input value="http://merchantsite.com/failure.html?q=fu" type="hidden" name="fu">
+                <input value="Checkout" type="submit">
+            </form>
         </div>
     </div>
 <div>
-  
-        
-
-
-
-
-       
-       
-       
-       
-       
-     
 
         @endif
-
-
-
 
 </div>
 @endsection
